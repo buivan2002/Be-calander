149c30d3af6599c02c74a1a86d8acaf267b3ded9
buivan2002 <buiquocanh1612@gmail.com>
2025-06-27 01:14:56 +0700

    cors

--- main.js
+++ main.js
@@ -5,12 +5,24 @@ const cookieParser = require("cookie-parser");
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: "http://localhost:3000",
-  credentials: true,               // Cho phép gửi cookie
-}));
-const { exec } = require('child_process');
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://calander-inky.vercel.app",
+];
 
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Cho phép nếu không có origin (VD: curl, postman) hoặc nằm trong danh sách
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS policy không cho phép truy cập từ origin này."));
+      }
+    },
+    credentials: true, // Cho phép cookie
+  })
+);
 
 // Routes
 const calanderRoutes = require('./routes/index');
